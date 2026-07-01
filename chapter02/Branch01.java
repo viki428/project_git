4b5b0342ffdbbeffca2aeeb8b128811d467015b4
seunghye <viki10635@gmail.com>
2026-07-01 10:02:31 +0900

    version4

--- chapter02/Branch01.java
+++ chapter02/Branch01.java
@@ -2,7 +2,7 @@ package chapter02;
 
 public class Branch01 {
     public Branch01(){
-        System.out.println("메인 브랜치에서 작업");
+        System.out.println("충돌 테스트");
         System.out.println("다음 브랜치에서 작업");
     }
 }
