0d8993df066461b752b04b0936cc7ce198ca11c3
seunghye <viki10635@gmail.com>
2026-07-01 10:04:18 +0900

    version4_g

--- chapter02/Branch01.java
+++ chapter02/Branch01.java
@@ -2,7 +2,7 @@ package chapter02;
 
 public class Branch01 {
     public Branch01(){
-        System.out.println("메인 브랜치에서 작업");
+        System.out.println("구글 브랜치에서 작업");
         System.out.println("다음 브랜치에서 작업");
     }
 }
